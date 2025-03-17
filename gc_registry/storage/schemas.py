b21ca552133db464b5180c21c6fb7c1cbfb0a06e
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-03-17 16:08:37 +0000

    Option 2 for storage SCR/SDR handling

--- gc_registry/storage/schemas.py
+++ gc_registry/storage/schemas.py
@@ -1,4 +1,5 @@
 import datetime
+from enum import Enum
 from typing import Union
 
 from sqlmodel import Field
@@ -6,65 +7,76 @@ from sqlmodel import Field
 from gc_registry import utils
 
 
-class StorageChargeRecordBase(utils.ActiveRecord):
-    charging_start_datetime: datetime.datetime = Field(
-        description="The UTC datetime at which the Storage Device began charging energy.",
+class FlowType(str, Enum):
+    CHARGING = "charging"
+    DISCHARGING = "discharging"
+
+
+class StorageChargeDischargeRecordBase(utils.ActiveRecord):
+    device_id: int = Field(
+        description="The unique ID of the Storage Device that is being charged or discharged.",
     )
-    charging_end_datetime: datetime.datetime = Field(
-        description="The UTC datetime at which the Storage Device ceased charging energy.",
+    flow_type: FlowType = Field(
+        description="The type of flow, either 'charging' or 'discharging'.",
     )
-    charging_energy: int = Field(
-        description="The quantity of energy in Watt-hours (Wh) that the Storage Device has been charged with.",
+    flow_start_datetime: datetime.datetime = Field(
+        description="The UTC datetime at which the Storage Device began charging or discharging energy.",
     )
-    charging_energy_source: str = Field(
-        description="""The energy source from which the Storage Device was charged, matching the energy source of the GC Bundles
-                       that were cancelled and allocated to the Storage Device.""",
+    flow_end_datetime: datetime.datetime = Field(
+        description="The UTC datetime at which the Storage Device ceased charging or discharging energy.",
     )
-    gc_issuance_id: int = Field(
-        description="The unique issuance ID of the GC Bundle that was cancelled and allocated to this SCR.",
+    flow_energy: float = Field(
+        description="The quantity of energy in Watt-hours (Wh) that the Storage Device has charged or discharged.",
     )
-    certificate_bundle_id_range_start: int = Field(
-        description="The start range ID of the GC Bundle that was cancelled and allocated to this SCR.",
+    flow_energy_source: str = Field(
+        description="The energy source from which the Storage Device was charged or discharged, matching the energy source of the GC Bundles \
+                       that were cancelled and allocated to the Storage Device."
     )
-    certificate_bundle_id_range_end: int = Field(
-        description="The end range ID of the GC Bundle that was cancelled and allocated to this SCR.",
+    gc_issuance_id: int | None = Field(
+        description="The unique issuance ID of the GC or SD-GC Bundle that was cancelled and allocated to this SCR/SDR.",
     )
-    scr_geographic_matching_method: str = Field(
-        description="Which type of geographic matching, as described in the EnergyTag GC Matching Standard, has been applied to allocated this SCR.",
+    certificate_bundle_id_range_start: int | None = Field(
+        description="The start range ID of the GC Bundle that was cancelled and allocated to this SCR/SDR.",
     )
-    sdr_allocation_id: int | None = Field(
-        description="When allocated, the unique ID of the Storage Discharge Record that has been allocated to this SCR. If blank, no SDR has been allocated to this SCR.",
-        foreign_key="storagedischargerecord.sdr_allocation_id",
+    certificate_bundle_id_range_end: int | None = Field(
+        description="The end range ID of the GC Bundle that was cancelled and allocated to this SCR/SDR.",
+    )
+    scr_allocation_methodology: str | None = Field(
+        description="The method by which the energy of the Storage Device was allocated to this SCR/SDR, for example: FIFO, LIFO, weighted average, or Storage Operator's discretion.",
+    )
+    efficiency_factor_methodology: str | None = Field(
+        description="The method by which the energy storage losses of the Storage Device were calculated.",
+    )
+    efficiency_factor_interval_start: datetime.datetime | None = Field(
+        description="""The UTC datetime from which the Storage Device calculates its effective efficiency factor for this SCR/SDR, based on total input and
+                       output energy over the interval specified. This field describes only the method proposed in the EnergyTag Standard, and is not mandatory.""",
+    )
+    efficiency_factor_interval_end: datetime.datetime | None = Field(
+        description="The UTC datetime to which the Storage Device calculates its effective efficiency factor for this SCR/SDR.",
     )
     is_deleted: bool = Field(default=False)
 
 
-class StorageDischargeRecordBase(utils.ActiveRecord):
-    discharging_start_datetime: datetime.datetime = Field(
-        description="The UTC datetime at which the Storage Device began discharging energy.",
+class AllocatedChargeDischargeRecordBase(utils.ActiveRecord):
+    device_id: int = Field(
+        description="The unique ID of the Storage Device that is being charged or discharged.",
     )
-    discharging_end_datetime: datetime.datetime = Field(
-        description="The UTC datetime at which the Storage Device ceased discharging energy.",
+    scr_allocation_id: int = Field(
+        description="The unique ID of the SCR that has been allocated to this matched record.",
+        foreign_key="storagechargerecord.id",
     )
-    discharge_energy: float = Field(
-        description="The quantity of energy in Watt-hours (Wh) that the Storage Device has discharged.",
+    sdr_allocation_id: int = Field(
+        description="The unique ID of the SDR that has been allocated to this matched record.",
+        foreign_key="storagedischargerecord.id",
     )
-    charging_energy_source: str = Field(
-        description="""The energy source from which the Storage Device was charged, matching the energy source of the GC Bundles
-                       that were cancelled and allocated to the Storage Device.""",
+    sdr_proportion: float = Field(
+        description="The proportion of the SDR that has been allocated to the linked SCR.",
     )
-    scr_allocation_methodology: str = Field(
-        description="The method by which the energy of the Storage Device was allocated to this SDR, for example: FIFO, LIFO, weighted average, or Storage Operator's discretion.",
+    gc_allocation_id: int = Field(
+        description="The unique ID of the cancelled GC Bundle that has been allocated to this matched record.",
     )
-    efficiency_factor_methodology: str = Field(
-        description="The method by which the energy storage losses of the Storage Device were calculated.",
-    )
-    efficiency_factor_interval_start: datetime.datetime | None = Field(
-        description="""The UTC datetime from which the Storage Device calculates its effective efficiency factor for this SDR, based on total input and
-                       output energy over the interval specified. This field describes only the method proposed in the EnergyTag Standard, and is not mandatory.""",
-    )
-    efficiency_factor_interval_end: datetime.datetime | None = Field(
-        description="The UTC datetime to which the Storage Device calculates its effective efficiency factor for this SDR.",
+    sdgc_allocation_id: int = Field(
+        description="The unique ID of the SD-GC Bundle that has been issued against this matched record.",
     )
     is_deleted: bool = Field(default=False)
 
@@ -121,9 +133,9 @@ class StorageActionResponse(StorageActionBase):
     )
 
 
-class SCRQueryResponse(StorageActionResponse):
-    filtered_scrs: Union[list[StorageChargeRecordBase], None]
+class StorageRecordQueryResponse(StorageActionResponse):
+    filtered_records: Union[list[StorageChargeDischargeRecordBase], None]
 
 
-class SDRQueryResponse(StorageActionResponse):
-    filtered_sdrs: Union[list[StorageDischargeRecordBase], None]
+class AllocatedChargeDischargeRecordQueryResponse(StorageActionResponse):
+    filtered_records: Union[list[AllocatedChargeDischargeRecordBase], None]
