b66d7c627f92a26d03c894c853a6f04e2caacb3d
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2025-07-22 22:01:51 +0100

    Routes to retrieving storage/allocation record by IDs

--- gc_registry/storage/routes.py
+++ gc_registry/storage/routes.py
@@ -4,7 +4,16 @@ from pathlib import Path
 
 import pandas as pd
 from esdbclient import EventStoreDBClient
-from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    File,
+    Form,
+    HTTPException,
+    Query,
+    UploadFile,
+    status,
+)
 from fastapi.responses import FileResponse
 from sqlmodel import Session, select
 
@@ -19,6 +28,7 @@ from gc_registry.logging_config import logger
 from gc_registry.storage.models import (
     AllocatedStorageRecord,
     StorageAction,
+    StorageRecord,
 )
 from gc_registry.storage.schemas import (
     AllocatedStorageRecordSubmissionResponse,
@@ -29,10 +39,15 @@ from gc_registry.storage.services import (
     create_allocated_storage_records_from_submitted_data,
     create_charge_records_from_metering_data,
     get_allocated_storage_records_by_device_id,
+    get_allocated_storage_records_by_id,
     get_device_ids_in_allocated_storage_records,
+    get_storage_records_by_id,
     issue_sdgcs_against_allocated_records,
 )
-from gc_registry.storage.validation import validate_storage_records
+from gc_registry.storage.validation import (
+    validate_access_to_devices,
+    validate_storage_records,
+)
 from gc_registry.user.models import User
 from gc_registry.user.validation import (
     validate_user_access,
@@ -156,6 +171,52 @@ async def submit_storage_records(
     return StorageRecordSubmissionResponse.model_validate(storage_submission_response)
 
 
+@router.get(
+    "/storage_records",
+    response_model=list[StorageRecord],
+    status_code=200,
+)
+async def get_storage_records_by_id_route(
+    storage_record_ids: list[int] = Query(
+        ...,
+        description="The IDs of the storage records to return.",
+        example=[1, 2, 3],
+    ),
+    current_user: User = Depends(get_current_user),
+    read_session: Session = Depends(db.get_read_session),
+):
+    """Return storage records for the specified IDs.
+
+    Verifies that the user has access to the devices associated with
+    the storage records prior to returning them.
+
+    Args:
+        storage_record_ids (list[int]): The IDs of the storage records to return.
+        current_user (User): The current user.
+        read_session (Session): The database session.
+
+    Returns:
+        list[StorageRecord]: The storage records for the specified IDs.
+    """
+    # Can be performed by both Storage Device owners and Storage Validators
+    if current_user.role != UserRoles.STORAGE_VALIDATOR:
+        validate_user_role(current_user, required_role=UserRoles.PRODUCTION_USER)
+
+    storage_records = get_storage_records_by_id(storage_record_ids, read_session)
+    if not storage_records:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No storage records found for the specified IDs.",
+        )
+
+    # Check that the user has access to the devices associated with the storage records
+    device_ids = set(record.device_id for record in storage_records)
+
+    validate_access_to_devices(device_ids, current_user, read_session)
+
+    return storage_records
+
+
 @router.post(
     "/allocated_storage_records",
     response_model=AllocatedStorageRecordSubmissionResponse,
@@ -222,6 +283,7 @@ async def create_storage_allocation(
 
     return AllocatedStorageRecordSubmissionResponse(
         total_records=len(allocated_storage_records),
+        record_ids=[record.id for record in allocated_storage_records],
         message="Allocation records created successfully.",
     )
 
@@ -283,6 +345,55 @@ def issue_SDGCs(
 
 
 @router.get(
+    "/allocated_storage_records",
+    response_model=list[AllocatedStorageRecord],
+    status_code=200,
+)
+def get_allocated_storage_records_by_id_route(
+    allocated_storage_record_ids: list[int] = Query(
+        ...,
+        description="The IDs of the allocated storage records to return.",
+        example=[1, 2, 3],
+    ),
+    current_user: User = Depends(get_current_user),
+    read_session: Session = Depends(db.get_read_session),
+):
+    """Return allocated storage records for the specified IDs.
+
+    Verifies that the user has access to the devices associated with
+    the allocated storage records prior to returning them.
+
+    Args:
+        allocated_storage_record_ids (list[int]): The IDs of the allocated storage records to return.
+        current_user (User): The current user.
+        read_session (Session): The database session.
+
+    Returns:
+        list[AllocatedStorageRecord]: The allocated storage records for the specified IDs.
+    """
+
+    # Can be performed by both Storage Device owners and Storage Validators
+    if current_user.role != UserRoles.STORAGE_VALIDATOR:
+        validate_user_role(current_user, required_role=UserRoles.PRODUCTION_USER)
+
+    allocated_storage_records = get_allocated_storage_records_by_id(
+        allocated_storage_record_ids, read_session
+    )
+    if not allocated_storage_records:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No allocated storage records found for the specified IDs.",
+        )
+
+    # Check that the user has access to the devices associated with the allocated storage records
+    device_ids = set(record.device_id for record in allocated_storage_records)
+
+    validate_access_to_devices(device_ids, current_user, read_session)
+
+    return allocated_storage_records
+
+
+@router.get(
     "/allocated_storage_records/{device_id}",
     response_model=list[AllocatedStorageRecord],
     status_code=200,
@@ -296,13 +407,6 @@ def get_allocated_storage_records(
 ):
     """Retrieve all allocated storage records."""
 
-    user_accounts = get_accounts_by_user_id(current_user.id, read_session)
-    if not user_accounts:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User does not have any associated accounts.",
-        )
-
     # check that the device_id is valid
     device_ids = get_device_ids_in_allocated_storage_records(read_session)
 
@@ -312,29 +416,7 @@ def get_allocated_storage_records(
             detail=f"Device with ID {device_id} not found in allocated storage records.",
         )
 
-    # check that the user has permission to access the device
-    if current_user.role not in [
-        UserRoles.ADMIN,
-        UserRoles.PRODUCTION_USER,
-        UserRoles.STORAGE_VALIDATOR,
-    ]:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="User must be an Admin, Production or Storage Validator to perform this action.",
-        )
-
-    # if the user is an Admin, Production User check that the device_id is associated with the user
-    if current_user.role in [UserRoles.ADMIN, UserRoles.PRODUCTION_USER]:
-        user_devices = []
-        for account in user_accounts:
-            account_devices = get_devices_by_account_id(account.id, read_session)
-            user_devices.extend(account_devices)
-
-        if device_id not in [device.id for device in user_devices]:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail=f"User does not have permission to access device with ID {device_id}.",
-            )
+    validate_access_to_devices([device_id], current_user, read_session)
 
     if not created_after:
         created_after = (datetime.datetime.now() - datetime.timedelta(days=7)).date()
